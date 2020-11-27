0213079e7e13ed39da07b23de79aee08b742fff3
igrolom <59706892+igrolom@users.noreply.github.com>
2020-11-27 15:39:02 +0300

    css animation practice

---
+++ script.js
@@ -0,0 +1,18 @@
+const bg = document.querySelector('h5').addEventListener('mouseover', changeBg);
+const bx = document.querySelectorAll('box');
+let bool = true;
+
+
+
+function changeBg() {
+    console.log('mouseover');
+    if (bool === true) {
+        document.querySelector('h5').style.backgroundColor = 'darkcyan';
+        bool = false;
+    } else if (bool === false) {
+        document.querySelector('h5').style.backgroundColor = 'rgb(3, 202, 202)';
+        bool = true;
+    }
+    bx.style.transform = 'rotate(15deg)';
+
+}
